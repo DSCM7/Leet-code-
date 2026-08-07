9b1c227137405baadf1b9096f0bd72bd6a20e587
DSCM7 <dscm2000515@gmail.com>
2026-08-07 13:45:19 +0530

    Add LeetCode 628 solution

--- Leetcode_easy/628.py
+++ Leetcode_easy/628.py
@@ -1,15 +1,8 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
-        size = len(nums)
+        nums.sort()
 
-        if size == 3:
-            return nums[0] * nums[1] * nums[2]
-
-        max_product = nums[0] * nums[1] * nums[2]
-
-        for i in range(size - 2):
-            p = nums[i] * nums[i+1] * nums[i+2]
-            if p > max_product:
-                max_product = p
-
-        return max_product
+        return max(
+            nums[-1] * nums[-2] * nums[-3],   
+            nums[0] * nums[1] * nums[-1]      
+        )
