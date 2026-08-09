b7b8d5b5c17c4a35a44d120efda181c34d42af26
DSCM7 <dscm2000515@gmail.com>
2026-08-09 19:21:23 +0530

    Add LeetCode 3754 solution.

--- Leetcode_easy/3754.py
+++ Leetcode_easy/3754.py
@@ -1,23 +1,24 @@
 class Solution:
     def sumAndMultiply(self, n: int) -> int:
-        s=str(n)
-        st=""
+        s = str(n)
+        st = ""
+
         if "0" not in s:
-            z=0
+            x = n
         else:
-            for i in s:
-                if i !="0":
-                    st=st+i
-            x=int(st)
-            t=x
-            sum=0
-            while(t!=0):
-                d=t%10
-                sum=sum+d
-                t=t//10
-            z=x*sum
-        return z
-            
-            
+            for ch in s:
+                if ch != "0":
+                    st += ch
+            if st == "":      # all digits were zero
+                return 0
+            x = int(st)
 
+        t = x
+        digit_sum = 0
 
+        while t != 0:
+            d = t % 10
+            digit_sum += d
+            t //= 10
+
+        return x * digit_sum
